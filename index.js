9ddd5e82939cffac7529286a2aa63d9fe3c16880
Rafael Bastiansch <rafaelbastiansch@gmail.com>
2017-12-21 15:14:24 -0200

    use arrow function and ternary operator

--- index.js
+++ index.js
@@ -2,52 +2,43 @@ let verify = true;
 const middleValue = 0;
 const speedIncrement = 0.2;
 
-function goDown(topPos){
+const goDown = topPos => {
   const rockBottom = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   let speed = 2;
   const middleValue = scrollY + ((topPos - scrollY) / 2);
 
-  const scroll = setInterval(() => {  
-    if(scrollY<middleValue){
-      speed = speed + speedIncrement;
-    }else if(scrollY>middleValue && speed > 1){
-      speed = speed - speedIncrement;
-    }
+  const scroll = setInterval(() => { 
+    speed = scrollY < middleValue ? speed + speedIncrement
+    : scrollY > middleValue && speed > 1 ? speed - speedIncrement
+    : speed;
 
     window.scroll(0, scrollY + Math.round(speed));
-    if(scrollY >= topPos || scrollY === rockBottom){
-      verify = true;
-      clearInterval(scroll);
-    }
+    verify = scrollY >= topPos || scrollY === rockBottom ? true : false;
+    verify ? clearInterval(scroll) : null;
   }, 1);  
 }
 
-function goUp(topPos){
+const goUp = topPos => {
   let speed = 2;            
   const middleValue = scrollY + ((topPos - scrollY) / 2);
 
   const scroll = setInterval(() => {
-    if(scrollY<middleValue && speed > 1){
-      speed = speed - speedIncrement;
-    }else if(scrollY>middleValue){
-      speed = speed + speedIncrement;
-    }
+    speed = scrollY < middleValue && speed > 1 ? speed - speedIncrement
+    : scrollY > middleValue ? speed + speedIncrement
+    : speed;
 
     window.scroll(0, scrollY - Math.round(speed));
-    if(scrollY <= topPos  || scrollY === 0){
-      verify = true;
-      clearInterval(scroll);                
-    }
+    verify = scrollY <= topPos || scrollY === 0 ? true : false;
+    verify ? clearInterval(scroll) : null;
   }, 1);
 }
 
-function scrolling(element){
+const scrolling = element => {
   const topPos = document.getElementById(element).offsetTop - 80;
-  if(topPos>scrollY && verify ){
-    verify = false;
-    goDown(topPos);
-  }else if(topPos<scrollY && verify){
-    verify = false;
-    goUp(topPos);
+  const scrollingDirection = () => {
+    topPos > scrollY ? goDown(topPos) : null;
+    topPos < scrollY ? goUp(topPos) : null;
+    return false;
   }
+  verify = verify ? scrollingDirection() : verify;
 }
